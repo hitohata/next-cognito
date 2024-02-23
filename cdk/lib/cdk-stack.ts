1351b3071809954df14b8392029fee51c6d1e9fc
hirohata <hirohatatro@gmail.com>
2024-02-23 11:39:58 -0800

    add resource

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,37 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {Stack} from "aws-cdk-lib";
-import {UserPool} from "aws-cdk-lib/aws-cognito";
-import * as appsync from "aws-cdk-lib/aws-appsync";
 import * as path from "node:path";
+import * as cdk from "aws-cdk-lib";
+import { Stack } from "aws-cdk-lib";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import { UserPool } from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const userPool = this.userPool(this);
+		const userPool = this.userPool(this);
+	}
 
-  }
+	userPool(stack: Stack) {
+		return new UserPool(stack, "UserPool", {
+			userPoolName: "next-cog",
+			selfSignUpEnabled: true,
+		});
+	}
 
-  userPool(stack: Stack) {
-    return new UserPool(stack, 'UserPool', {
-      userPoolName: "next-cog",
-      selfSignUpEnabled: true,
-    })
-  }
-
-  appSync(stack: Stack, userPool: UserPool) {
-    return new appsync.GraphqlApi(stack, 'AppSync', {
-      name: 'next-app-api',
-      definition: appsync.Definition.fromFile(path.join(__dirname, "../schema.graphql")),
-      authorizationConfig: {
-        defaultAuthorization: {
-          authorizationType: appsync.AuthorizationType.USER_POOL,
-          userPoolConfig: {
-            userPool
-          }
-        }
-      }
-    })
-  }
+	appSync(stack: Stack, userPool: UserPool) {
+		return new appsync.GraphqlApi(stack, "AppSync", {
+			name: "next-app-api",
+			definition: appsync.Definition.fromFile(
+				path.join(__dirname, "../schema.graphql"),
+			),
+			authorizationConfig: {
+				defaultAuthorization: {
+					authorizationType: appsync.AuthorizationType.USER_POOL,
+					userPoolConfig: {
+						userPool,
+					},
+				},
+			},
+		});
+	}
 }
