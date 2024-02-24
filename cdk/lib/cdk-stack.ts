06adea78ca5be9790ec14715dd39289c9288897c
hirohata <hirohatatro@gmail.com>
2024-02-23 20:16:47 -0800

    updated cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,29 +1,55 @@
 import * as path from "node:path";
 import * as cdk from "aws-cdk-lib";
-import { Stack } from "aws-cdk-lib";
+import {RemovalPolicy} from "aws-cdk-lib";
 import * as appsync from "aws-cdk-lib/aws-appsync";
-import { UserPool } from "aws-cdk-lib/aws-cognito";
-import { Construct } from "constructs";
+import {AccountRecovery, StringAttribute, UserPool, VerificationEmailStyle} from "aws-cdk-lib/aws-cognito";
+import { Construct} from "constructs";
 
 export class CdkStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		const userPool = this.userPool(this);
+		const userPool = this.userPool();
+		this.userPoolClient(userPool);
+        this.appSync(userPool);
 	}
 
-	userPool(stack: Stack) {
-		return new UserPool(stack, "UserPool", {
+	userPool() {
+		return new UserPool(this, "UserPool", {
 			userPoolName: "next-cog",
 			selfSignUpEnabled: true,
+			signInAliases: {
+				email: true,
+				username: false,
+				phone: false
+			},
+			autoVerify: { email: true },
+			userVerification: {
+				emailStyle: VerificationEmailStyle.CODE,
+			},
+			customAttributes: {
+				name: new StringAttribute({ minLen: 1, maxLen: 256, mutable: true }),
+			},
+			accountRecovery: AccountRecovery.EMAIL_ONLY,
+			removalPolicy: RemovalPolicy.DESTROY
 		});
 	}
 
-	appSync(stack: Stack, userPool: UserPool) {
-		return new appsync.GraphqlApi(stack, "AppSync", {
+	userPoolClient(userPool: UserPool) {
+		userPool.addClient("next-app-client", {
+			userPoolClientName: "next-front",
+			authFlows: {
+				userPassword: true,
+				userSrp: true,
+			},
+		});
+	}
+
+	appSync(userPool: UserPool) {
+		return new appsync.GraphqlApi(this, "AppSync", {
 			name: "next-app-api",
 			definition: appsync.Definition.fromFile(
-				path.join(__dirname, "../schema.graphql"),
+				path.join(__dirname, "../graphql/schema.graphql"),
 			),
 			authorizationConfig: {
 				defaultAuthorization: {
