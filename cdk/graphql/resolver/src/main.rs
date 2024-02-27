989d61a7ca5dd36d3b2a6206cff326e6f4ef413b
hirohata <hirohatatro@gmail.com>
2024-02-26 17:24:35 -0800

    updated lambda function

--- cdk/graphql/resolver/src/main.rs
+++ cdk/graphql/resolver/src/main.rs
@@ -1,20 +1,13 @@
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 
 use serde::{Deserialize, Serialize};
-
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
-#[derive(Deserialize)]
-struct Request {
-    command: String,
-}
+use serde_json::{json, Value};
 
 /// This is a made-up example of what a response structure may look like.
 /// There is no restriction on what it can be. The runtime requires responses
 /// to be serialized into json. The runtime pays no attention
 /// to the contents of the response payload.
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct Response {
     id: String,
     version: String,
@@ -25,15 +18,15 @@ struct Response {
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 /// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(_event: LambdaEvent<Request>) -> Result<Vec<Response>, Error> {
+async fn function_handler(_event: LambdaEvent<Value>) -> Result<Value, Error> {
     // Extract some useful info from the request
     // let command = event.payload.command;
 
     // Prepare the response
-    let resp = vec![Response {
+    let resp = json!(vec![Response {
         id: String::from("1"),
         version: String::from("version"),
-    }];
+    }]);
 
     // Return `Response` (it will be serialized to JSON automatically by the runtime)
     Ok(resp)
